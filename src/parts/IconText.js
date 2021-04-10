a70441d06fd224a6fe9e88f0f5d458594d6bd390
maulanairfanf <maulanairfanf@gmail.com>
2021-04-10 14:57:51 +0700

    intiial

---
+++ src/parts/IconText.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Button from "elements/Button";
+
+const IconText = (props) => {
+  return (
+    <Button
+      className={`brand-text-icon ${props ? "mx-auto" : ""}`}
+      href=""
+      type="link"
+    >
+      Stay<span className="text-gray-900">cation.</span>
+    </Button>
+  );
+};
+
+export default IconText;
